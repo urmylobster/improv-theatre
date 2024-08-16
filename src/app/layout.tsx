3397d0720844ff8f313b2f36b4e7b05806d082ea
urmylobster <alexander2720@163.com>
2024-08-16 08:43:32 +0800

    style:编辑首页

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <title>Improvsational Theatre</title>
       <body className={inter.className}>{children}</body>
     </html>
   );
